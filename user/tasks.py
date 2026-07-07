6490718ab26430f008484d229b22b3eb08fd42b4
JavohirBoboxonov <javohirboboxonov08@gmail.com>
2026-07-07 11:54:23 +0500

    celery added in user

--- user/tasks.py
+++ user/tasks.py
@@ -1,19 +1,10 @@
-import logging
-from celery import shared_task
 from django.core.mail import send_mail
-from decouple import config
-logger = logging.getLogger(__name__)
+from django.conf import settings
+from celery import shared_task
+
 @shared_task
 def send_verification_code(email, code):
-    try:
-        is_send = send_mail(
-            subject="AloqaSphere",
-            message=f"Sizning registrasiya kodingiz {code}",
-            from_email=config("EMAIL_HOST_USER"),
-            recipient_list=[email],
-        )
-        return is_send
-    
-    except Exception as e:
-        logger.error(f"Xatolik {email}:{e}")
-        return 0
+    subject = "Tasdiqlash kodi"
+    message = f"Sizning tasdiqlash kodingiz: {code}"
+    from_email = settings.EMAIL_HOST_USER 
+    send_mail(subject, message, from_email, [email])
